fe496ea4889aafd8bd48a67171990697431a4055
mc-gomes <bigmath2001@hotmail.com>
2024-09-16 17:53:22 -0300

    criacao de componente para pagina de Favoritos

--- books-api/src/app/app.component.ts
+++ books-api/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterModule } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, RouterModule],
+  imports: [HomeComponent, RouterModule, RouterLink, RouterOutlet],
   template: `
     <main>
+      <header class="brand-name">
+        <nav>
+          <a [routerLink]="['/']">Página Inicial</a>
+          <span>|</span>
+          <a [routerLink]="['/favorites']">Favoritos</a>
+        </nav>
+      </header>
       <section class="content">
         <router-outlet></router-outlet>
       </section>
--- books-api/src/app/routes.ts
+++ books-api/src/app/routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
 
 const routeConfig: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home page',
+  },
+  {
+    path: 'favorites',
+    component: FavoritesComponent,
+    title: 'Página de favoritos',
   },
 ];
 export default routeConfig;
